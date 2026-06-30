9a56ad80dc90fcff35f7cb1f83c5385cb585571e
Reshmanth CH <chilukurireshmanth007@gmail.com>
2026-06-30 17:32:37 +0530

    feat: configure application routing

---
+++ client/src/routes/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Dashboard from "../pages/Dashboard";
+import NotFound from "../pages/NotFound";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
