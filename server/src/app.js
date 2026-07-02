5753c6529a772d108f13fbd4f022721e7414fd27
Reshmanth CH <chilukurireshmanth007@gmail.com>
2026-07-02 15:53:45 +0530

    feat: implement centralized error handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import cors from "cors";
+
+import errorHandler from "./middleware/errorMiddleware.js";
+import ApiError from "./utils/ApiError.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route Not Found
+app.use((req, res, next) => {
+  next(new ApiError(404, "Route not found"));
+});
+
+// Global Error Handler
+app.use(errorHandler);
 
 export default app;
